6ffa0fa70bdca503c6c00d4f752ed9a75d8f38b8
Himanshu-Kushwaha <himanshukushwaha@outlook.com>
2016-12-13 18:34:56 +0530

    make it better

--- facebook.js
+++ facebook.js
@@ -60,11 +60,11 @@ const quick = (recipientId, msg, cb) => {
                 quick_replies: [{
                     content_type: "text",
                     title: "Plot",
-                    payload: msg
+                    //payload: msg
                 }, {
                     content_type: "text",
                     title: "Genre",
-                    payload: "msg"
+                    // payload: "msg"
 
                 }, ]
             }
