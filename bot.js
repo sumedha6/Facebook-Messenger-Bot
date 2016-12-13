6f7d57e0cf4cc847c32ee9e99c046355c0529fff
Himanshu-Kushwaha <himanshukushwaha@outlook.com>
2016-12-13 21:35:36 +0530

    make it better

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ const actions = {
             return;
         }
         const recipientId = context._fbid_;
-        if (recipientId && (context.feature === 'plot' || context.feature === 'genre')) {
+        if (recipientId) {
             const sendFBQuick = () => {
                 FB.quick(recipientId, message, (err, data) => {
                     if (err) {
